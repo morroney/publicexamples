f0c2e5b17fd9c10f7652f6b3f3a0a944ce0ad5e7
Dan <morroney@users.noreply.github.com>
2023-04-25 19:26:19 -1000

    Create deeplinkSquare.js

---
+++ deeplinkSquare.js
@@ -0,0 +1,29 @@
+import {Linking} from "react-native"
+
+
+const startDeepLink = async () => {
+
+
+      const squareAppID = 'APP_ID' // application production id. 
+      const idpAppCallback = 'CALLBACK_URL' // defined in application dashboard > Oauth
+      const squareCatalogItemID = 'ITEM_ID' // id of item, can be retrieved from catalog endpoint
+      const locationID = 'LOCATION_ID' // for multiple locations
+
+      const dataPackage = {
+                                      id: squareCatalogItemID,
+                                      client_id: squareAppID, 
+                                      callback_url: idpAppCallback 
+                                    }
+
+      const encodeProduct = encodeURIComponent(JSON.stringify(dataPackage))
+
+      const url = `square-commerce-v1://catalog/details?data=${encodeProduct}`
+   // const url = `square-commerce-v1://payment/create?data=${encodeProduct}`; //tried this too didn't work. 
+
+      
+      Linking.openURL(url);
+  
+    }
+
+
+// From here, startDeepLink() would be fired from a button press or something similar in the rendered return { } function that would come next. 
